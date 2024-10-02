123ba1e63391b29824886414261b20db0e84bf98
Matthias Kersting <matthias@kersting.xyz>
2024-10-02 14:20:38 +0200

    Initial revision.

---
+++ LawfulMod/API/CivicsController.cs
@@ -0,0 +1,33 @@
+﻿using Eco.Core.Systems;
+using Eco.Gameplay.Civics.Laws;
+using Eco.Gameplay.Civics.Misc;
+using Eco.Gameplay.Objects;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LawfulMod.API
+{
+    [Route("api/v1/lawful")]
+    public class CivicsController : Controller
+    {
+        [HttpGet("laws")]
+        [AllowAnonymous]
+        public int[] GetStrings()
+        {
+            CivicsUtils.
+            return Registrars.Get<Law>().All().Select(l => l.Id).ToArray();
+        }
+
+        [HttpGet("section")]
+        [AllowAnonymous]
+        public string[] GetSections(int lawId)
+        {
+            return Registrars.Get<Law>().FirstOrDefault(l => l.Id == lawId)?.Sections.Select(s => s.Title).ToArray() ?? Array.Empty<string>();
+        }
+    }
+}
---
+++ LawfulMod/LawfulPlugin.cs
@@ -0,0 +1,50 @@
+﻿using Eco.Core.Plugins.Interfaces;
+using Eco.Core.Utils;
+using Eco.Shared.Logging;
+using Eco.Shared.Localization;
+
+namespace LawfulMod
+{
+    public class LawfulPlugin : IInitializablePlugin, IModKitPlugin, IWebPlugin
+    {
+        public string GetCategory()
+        {
+            return "Civics";
+        }
+
+        public string? GetEmbeddedResourceNamespace()
+        {
+            return "LawfulMod.www";
+        }
+
+        public string? GetFontAwesomeIcon()
+        {
+            return null;
+        }
+
+        public LocString GetMenuTitle()
+        {
+            return new LocString("Law Manager");
+        }
+
+        public string? GetPluginIndexUrl()
+        {
+            return "LawfulPlugin/index.html";
+        }
+
+        public string? GetStaticFilesPath()
+        {
+            return null;
+        }
+
+        public string GetStatus()
+        {
+            return "Idle";
+        }
+
+        public void Initialize(TimedTask timer)
+        {
+            Log.WriteLine(new LocString("LawfulPlugin initialized"));
+        }
+    }
+}
