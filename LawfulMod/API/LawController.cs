66bcfc7e55a4efae4328675547c04103a223e7c2
Matthias Kersting <matthias@kersting.xyz>
2024-10-11 21:45:19 +0200

    Don't fail if HostObject is null.

--- LawfulMod/API/LawController.cs
+++ LawfulMod/API/LawController.cs
@@ -1,5 +1,6 @@
 ﻿using Eco.Core.Systems;
 using Eco.Gameplay.Civics.Laws;
+using Eco.Gameplay.Civics.Misc;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,13 +9,13 @@ namespace LawfulMod.API
     [Route("api/v1/lawful/law")]
     public class LawController : BaseController
     {
-        public record LawDto(int Id, string Title, string Description, string Creator, String State, string Settlement, string HostObject);
+        public record LawDto(int Id, string Title, string Description, string Creator, String State, string Settlement, string? HostObject);
 
         [HttpGet]
         [AllowAnonymous]
         public LawDto[] GetLaws()
         {
-            var laws = Registrars.Get<Law>().All();
+            var laws = Registrars.Get<Law>().Where(l => l.Active());
             return laws.Select(l => GetDto(l)).ToArray();
         }
 
@@ -35,7 +36,7 @@ namespace LawfulMod.API
 
         private LawDto GetDto(Law law)
         {
-            return new LawDto(law.Id, law.Name, law.UserDescription, law.Creator.Name, law.State.ToString(), law.Settlement.Name, law.HostObject.Object.Name);
+            return new LawDto(law.Id, law.Name, law.UserDescription, law.Creator.Name, law.State.ToString(), law.Settlement.Name, law.HostObject.Object?.Name);
         }
     }
 }
